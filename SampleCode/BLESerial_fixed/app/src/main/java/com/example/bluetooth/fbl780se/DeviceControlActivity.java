ceadcd0048b272852d4616425ad00c0b537042fb
Woohyun Shin <cpu344@gmail.com>
2019-04-09 14:24:07 +0900

    Reverted unintended change in sample code

--- SampleCode/BLESerial_fixed/app/src/main/java/com/example/bluetooth/fbl780se/DeviceControlActivity.java
+++ SampleCode/BLESerial_fixed/app/src/main/java/com/example/bluetooth/fbl780se/DeviceControlActivity.java
@@ -597,6 +597,14 @@ public class DeviceControlActivity extends AppCompatActivity {
     }
 
 
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        return intentFilter;
+    }
 
 
 
