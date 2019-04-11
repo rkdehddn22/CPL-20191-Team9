3e2623dafad36f71dfd9b06f4079543011857202
Woohyun Shin <cpu344@gmail.com>
2019-04-12 03:21:04 +0900

    Splash screen

--- AndroidApp/app/src/main/java/cdp/t9/SplashActivity.java
+++ AndroidApp/app/src/main/java/cdp/t9/SplashActivity.java
@@ -3,11 +3,14 @@ package cdp.t9;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
     SharedPreferences sp;
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +20,19 @@ public class SplashActivity extends AppCompatActivity {
         // initialize preference
         sp = getSharedPreferences("cdp.t9.settings", Activity.MODE_PRIVATE);
 
-        // check if pin code is set
-        if (sp.getString("pin_code", null) != null) {
-            // proceed to pin code activity
-        } else {
-            // proceed to main activity
-            startActivity(new Intent(this, MainActivity.class));
-            finish();
-        }
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // check if pin code is set
+                if (sp.getString("pin_code", null) != null) {
+                    // proceed to pin code activity
+                } else {
+                    // proceed to main activity
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        }, 3000);
     }
 }
